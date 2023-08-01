419a8fedc39000f324fae1956fa005c6fd8b4a08
akane-minami <akane03730913@gmail.com>
2023-08-01 18:10:17 +0900

    add submission: 2023/08/01 18:10:17

---
+++ submissions/abc312/a.py
@@ -0,0 +1,19 @@
+S = input()
+flag = "No"
+
+if S == "ACE":
+  flag = "Yes"
+elif S == "BDF":
+  flag = "Yes"
+elif S == "CEG":
+  flag = "Yes"
+elif S == "DFA":
+  flag = "Yes"
+elif S == "EGB":
+  flag = "Yes"
+elif S == "FAC":
+  flag = "Yes"
+elif S == "GBD":
+  flag = "Yes"
+
+print(flag)
